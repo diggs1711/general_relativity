b29487a3eae40908056a8bd7157fa57dad6bca17
david.higgins <david.higgins7@ucdconnect.ie>
2022-11-16 15:52:07 +0000

    Tidy up

--- main.py
+++ main.py
@@ -1,97 +1,100 @@
-from sympy import symbols, Function, Pow, MutableDenseNDimArray, diff, init_printing, pprint, simplify
+from sympy import (Function, MutableDenseNDimArray, Pow, diff, init_printing,
+                   pprint, simplify, symbols)
 from sympy.physics.units.quantities import Quantity
 
 init_printing()
 
-symbols = symbols('t x y z')
+'''Constants'''
+COORDINATES = symbols('t x y z')
 c = Quantity('c')
 a = Function('a')
-
-'''Constants'''
-COORDINATES = 4
+r = Quantity('r')
+NUM_COORDINATES = len(COORDINATES)
 
 
 def christoffel_symbols(metric: MutableDenseNDimArray):
-    values = MutableDenseNDimArray.zeros(COORDINATES, COORDINATES, COORDINATES)
-    for a in range(0, COORDINATES):
-        for b in range(0, COORDINATES):
-            for c in range(0, COORDINATES):
+    values = MutableDenseNDimArray.zeros(
+        NUM_COORDINATES, NUM_COORDINATES, NUM_COORDINATES)
+    for a in range(0, NUM_COORDINATES):
+        for b in range(0, NUM_COORDINATES):
+            for c in range(0, NUM_COORDINATES):
                 val = 0
-                for d in range(0, COORDINATES):
+                for d in range(0, NUM_COORDINATES):
                     if (metric[a, d] != 0):
                         val += 0.5*(1/metric[a, d]) * \
-                            (diff(metric[d, b], symbols[c]) + diff(metric[d, c],
-                             symbols[b]) - diff(metric[b, c], symbols[d]))
+                            (diff(metric[d, b], COORDINATES[c]) + diff(metric[d, c],
+                             COORDINATES[b]) - diff(metric[b, c], COORDINATES[d]))
                 values[a, b, c] = val
-    return values
+    return simplify(values)
 
 
 def riemann_tensor(christoffel_symbols):
     values = MutableDenseNDimArray.zeros(
-        COORDINATES, COORDINATES, COORDINATES, COORDINATES)
-    for d in range(0, COORDINATES):
-        for a in range(0, COORDINATES):
-            for b in range(0, COORDINATES):
-                for c in range(0, COORDINATES):
+        NUM_COORDINATES, NUM_COORDINATES, NUM_COORDINATES, NUM_COORDINATES)
+    for d in range(0, NUM_COORDINATES):
+        for a in range(0, NUM_COORDINATES):
+            for b in range(0, NUM_COORDINATES):
+                for c in range(0, NUM_COORDINATES):
                     val1 = 0
-                    for e in range(0, COORDINATES):
+                    val2 = 0
+                    for e in range(0, NUM_COORDINATES):
                         val1 += christoffel_symbols[e, a,
                                                     c]*christoffel_symbols[d, e, b]
-                    val2 = 0
-                    for e in range(0, COORDINATES):
                         val2 += christoffel_symbols[e, a,
                                                     b]*christoffel_symbols[d, e, c]
-
-                    values[d, a, b, c] += diff(christoffel_symbols[d, a, c], symbols[b]) - diff(
-                        christoffel_symbols[d, a, b], symbols[c]) + val1 - val2
-
-    return values
+                    values[d, a, b, c] += diff(christoffel_symbols[d, a, c], COORDINATES[b]) - diff(
+                        christoffel_symbols[d, a, b], COORDINATES[c]) + val1 - val2
+    return simplify(values)
 
 
 def ricci_tensor(riemann_tensor):
-    values = MutableDenseNDimArray.zeros(COORDINATES, COORDINATES)
-    for a in range(0, COORDINATES):
-        for b in range(0, COORDINATES):
+    values = MutableDenseNDimArray.zeros(NUM_COORDINATES, NUM_COORDINATES)
+    for a in range(0, NUM_COORDINATES):
+        for b in range(0, NUM_COORDINATES):
             val = 0
-            for c in range(0, COORDINATES):
-                val += riemann_tensor[c, a, c, b]
+            for c in range(0, NUM_COORDINATES):
+                val += riemann_tensor[c, a, b, c]
             values[a, b] = val
-    return values
+    return simplify(values)
 
 
 def ricci_scalar(ricci_tensor, metric):
     result = 0
-    for a in range(0, COORDINATES):
-        for b in range(0, COORDINATES):
+    for a in range(0, NUM_COORDINATES):
+        for b in range(0, NUM_COORDINATES):
             if metric[a, b] != 0:
                 result += (1/metric[a, b])*(ricci_tensor[a, b])
-    return result
+    return simplify(result)
 
 
 def einstein_tensor(ricci_tensor, ricci_scalar, metric):
-    result = MutableDenseNDimArray.zeros(COORDINATES, COORDINATES)
-    for a in range(0, COORDINATES):
-        for b in range(0, COORDINATES):
+    result = MutableDenseNDimArray.zeros(NUM_COORDINATES, NUM_COORDINATES)
+    for a in range(0, NUM_COORDINATES):
+        for b in range(0, NUM_COORDINATES):
             result[a, b] = ricci_tensor[a, b] - \
                 0.5*(ricci_scalar*(metric[a, b]))
-    return result
+    return simplify(result)
 
 
-metric = MutableDenseNDimArray.zeros(COORDINATES, COORDINATES)
+def calculate_metric():
+    t = COORDINATES[0]
+    metric = MutableDenseNDimArray.zeros(NUM_COORDINATES, NUM_COORDINATES)
+    metric[0, 0] = Pow(c, 2)
+    metric[1, 1] = -Pow(a(t), 2)
+    metric[2, 2] = -Pow(a(t), 2)
+    metric[3, 3] = -Pow(a(t), 2)
+    return metric
 
-t = symbols[0]
-metric[0, 0] = Pow(c, 2)
-metric[1, 1] = -Pow(a(t), 2)
-metric[2, 2] = -Pow(a(t), 2)
-metric[3, 3] = -Pow(a(t), 2)
 
+if __name__ == '__main__':
+    metric = calculate_metric()
 
-chris_syms = christoffel_symbols(metric)
-# pprint(chris_syms)
-riemann = riemann_tensor(chris_syms)
-# pprint(riemann)
-r_tensor = ricci_tensor(riemann)
-r_scalar = ricci_scalar(r_tensor, metric)
-# pprint(simplify(r_scalar))
-einstein = einstein_tensor(r_tensor, r_scalar, metric)
-pprint(simplify(einstein[0, 0]))
+    chris_syms = christoffel_symbols(metric)
+    riemann = riemann_tensor(chris_syms)
+    r_tensor = ricci_tensor(riemann)
+    r_scalar = ricci_scalar(r_tensor, metric)
+    e_tensor = einstein_tensor(r_tensor, r_scalar, metric)
+
+    pprint(chris_syms)
+    pprint(r_tensor)
+    pprint(e_tensor)
